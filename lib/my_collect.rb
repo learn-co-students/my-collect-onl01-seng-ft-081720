75afd4912e58b5642a00a0ec72a7412c14157eee
NickRylander <nick.rylander@outlook.com>
2020-08-20 10:18:20 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def my_collect(collection)
+    i = 0
+    lang = []
+        while i < collection.length
+            lang << yield(collection[i])
+            i += 1
+        end
+    lang
+end
